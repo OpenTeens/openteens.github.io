5228f5a633dbc307b0eea9d8658b6fb364afd4d2
Jinyuan Huang <88757735+BernieHuang2008@users.noreply.github.com>
2023-09-05 07:03:34 +0800

    Update lang-color.js

--- js/lang-color.js
+++ js/lang-color.js
@@ -1,7 +1,7 @@
 // copied from https://github.com/doda-zz/github-language-colors
 
 lang_colors = { 
-     "unknown": "#555",
+     "unknown": "#000",
      "mercury": "#ff2b2b",  
      "typescript": "#2b7489",  
      "purebasic": "#5a6986",  
