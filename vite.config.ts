51fd41b82a06c6e274f72555906ecd5de7bc4149
Acbox liu <850625057@qq.com>
2024-11-24 21:18:26 +0800

    chore

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import components from "unplugin-vue-components/vite";
-// import autoImport from "unplugin-auto-import/vite";
+import autoImport from "unplugin-auto-import/vite";
 import Icons from "unplugin-icons/vite";
 import IconsResolver from "unplugin-icons/resolver";
 // https://vite.dev/config/
@@ -11,15 +11,15 @@ export default defineConfig({
     components({
       resolvers: [IconsResolver({})],
     }),
-    // autoImport({
-    //   include: [
-    //     /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
-    //     /\.vue$/,
-    //     /\.vue\?vue/, // .vue
-    //     /\.md$/, // .md
-    //   ],
-    //   imports: ["vue", "vue-router"],
-    // }),
+    autoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+      ],
+      imports: ["vue", "vue-router"],
+    }),
     Icons(),
   ],
 });
